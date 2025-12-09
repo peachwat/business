3af5d39660a3ab93696b54c8fecb1ec86914d315
nikitaksk <nikitaksk1@icloud.com>
2025-12-09 20:37:38 +0100

    interative parser upgrade, main menu for verefication added

--- parser/parse_products.py
+++ parser/parse_products.py
@@ -19,7 +19,6 @@ import csv
 def human_wait(base: float = 3.0, variance: float = 2.0):
     time.sleep(random.uniform(base - variance, base + variance))
 
-
 def accept_cookies(driver: uc.Chrome):
     try:
         wait = WebDriverWait(driver, 10)
@@ -30,18 +29,27 @@ def accept_cookies(driver: uc.Chrome):
         logging.error("NoSuchElementException while waiting for cookies to become available")
 
 
-ID_OFFSET = 2
-
-
 def parse(driver: uc.Chrome, limit_of_categories: int = None, limit_of_subcategories: int = None,
-          limit_of_groups: int = None) -> List[Dict]:
+          limit_of_groups: int = None, limit_of_products_in_group: int = None) -> List[Dict]:
     product_categories_data = []
     top_category_elements = driver.find_elements(By.XPATH, "//a[contains(@class, 'dropdown-item') and @data-depth='0']")
 
+
+    # workaround to not parse premium products with different layout and styling
+    categories_to_skip = ['Herbata premium']
+    groups_to_skip = ['Herbata Biała', 'Herbaty japońskie']
+
     for id, top_category_element in enumerate(top_category_elements[:limit_of_categories]):
+        category_name : str = top_category_element.get_attribute('textContent').strip()
+        if (category_name in categories_to_skip):
+            logging.info(f"Skipping category: {category_name}")
+            continue
+
+        logging.info(f"Fetching category: {category_name}")
+
         top_category_data = {
             "id": None,
-            "name": top_category_element.get_attribute('textContent').strip(),
+            "name": category_name,
             "link": top_category_element.get_attribute('href'),
             "active": 1,
             "parent": "Home",
@@ -59,22 +67,36 @@ def parse(driver: uc.Chrome, limit_of_categories: int = None, limit_of_subcatego
                 "active": 1,
                 "parent": top_category_data["name"],
                 "is_root": 0,
-                "groups": []
             }
+
+            logging.info(f"Fetching subcategory: {subcategory_data['name']}")
 
             group_elements = subcategory_element.find_elements(By.XPATH,
                                                                "following-sibling::div//a[contains(@class, 'dropdown-item') and @data-depth='2']")
-            for group_element in group_elements[:limit_of_groups]:
-                group_data = {
-                    "id": None,
-                    "name": group_element.get_attribute('textContent').strip(),
-                    "link": group_element.get_attribute('href'),
-                    "active": 1,
-                    "parent": subcategory_data["name"],
-                    "is_root": 0,
-                    "products": []
-                }
-                subcategory_data["groups"].append(group_data)
+
+            if group_elements:
+                subcategory_data['groups'] = []
+                for group_element in group_elements[:limit_of_groups]:
+                    group_name = group_element.get_attribute('textContent').strip()
+                    if group_name in groups_to_skip:
+                        logging.info(f"Skipping group {group_name}")
+                        continue
+
+                    logging.info(f"Fetching group: {group_name}")
+
+                    group_data = {
+                        "id": None,
+                        "name": group_element.get_attribute('textContent').strip(),
+                        "link": group_element.get_attribute('href'),
+                        "active": 1,
+                        "parent": subcategory_data["name"],
+                        "is_root": 0,
+                        "products": []
+                    }
+                    subcategory_data["groups"].append(group_data)
+            else:
+                subcategory_data['products'] = []
+
             top_category_data["subcategories"].append(subcategory_data)
         product_categories_data.append(top_category_data)
 
@@ -113,6 +135,39 @@ def parse(driver: uc.Chrome, limit_of_categories: int = None, limit_of_subcatego
             categories_writer = csv.writer(categories_csv, delimiter=';')
             categories_writer.writerow(categories_csv_headers)
             product_writer.writerow(products_csv_headers)
+
+            def process_and_write_products(product_container, link, limit):
+                logging.info(f"Parsing products from {link}")
+                product_summaries = parse_products_from_page(driver, link)
+                detailed_products = []
+                for summary in product_summaries[:limit]:
+                    product_link = summary['link']
+                    detailed_info = parse_product_page(driver, product_link)
+                    summary.update(detailed_info)
+                    detailed_products.append(summary)
+                product_container["products"] = detailed_products
+
+                for product in detailed_products:
+                    if product['name'] not in unique_product_names:
+                        unique_product_names.add(product['name'])
+                        categories_entry = ','.join([temp_cat['name'] for temp_cat in product["categories"]])
+                        images = ["http://localhost/" + img_path for img_path in product["images_path"]]
+                        product_writer.writerow(
+                            [product["id"],
+                             product["active"],
+                             product["name"],
+                             categories_entry,  # categories
+                             product["price_netto"],  # Price tax excluded
+                             1,  # "Tax rules ID"
+                             random.randint(1, 100),  # "Quantity",
+                             product["short_description"],  # "Summary"
+                             product["text_description"],  # "Description"
+                             1,  # Show price (0 = No, 1 = Yes)
+                             ", ".join(images),  # Image URLs (x,y,z...)
+                             1,  # Usuń istniejące zdjęcia (0 = Nie, 1 = Tak)
+                             ]
+                        )
+
             for category in product_categories_data:
                 categories_writer.writerow(
                     [category["id"], category["active"], category["name"], category["parent"], category["is_root"]])
@@ -121,41 +176,14 @@ def parse(driver: uc.Chrome, limit_of_categories: int = None, limit_of_subcatego
                         [subcategory["id"], subcategory["active"], subcategory["name"], subcategory["parent"],
                          subcategory["is_root"]]
                     )
-                    for group in subcategory["groups"]:
-                        categories_writer.writerow(
-                            [group["id"], group["active"], group["name"], group["parent"], group["is_root"]]
-                        )
-                        link = group["link"]
-                        logging.info(f"Parsing products in group {link}")
-                        product_summaries = parse_products_from_page(driver, link)
-                        detailed_products = []
-                        for summary in product_summaries[:4]: #TODO: Disable limit
-                            product_link = summary['link']
-                            detailed_info = parse_product_page(driver, product_link)
-                            summary.update(detailed_info)
-                            detailed_products.append(summary)
-                        group["products"] = detailed_products
-                        for product in detailed_products:
-                            if product['name'] not in unique_product_names:
-                                unique_product_names.add(product['name'])
-                                categories_entry = ','.join([temp_cat['name'] for temp_cat in product["categories"]])
-                                images = ["http://localhost/" + img_path for img_path in product["images_path"]]
-                                product_writer.writerow(
-                                    [product["id"],
-                                     product["active"],
-                                     product["name"],
-                                     categories_entry, # categories
-                                     product["price_netto"], # Price tax excluded
-                                     1, #"Tax rules ID"
-                                     random.randint(1, 100),# "Quantity",
-                                     product["short_description"], #"Summary"
-                                     product["text_description"], # "Description"
-                                     1, # Show price (0 = No, 1 = Yes)
-                                     ", ".join(images),  # Image URLs (x,y,z...)
-                                     1, # Usuń istniejące zdjęcia (0 = Nie, 1 = Tak)
-                                     ]
-                                )
-
+                    if "groups" in subcategory and subcategory["groups"]:
+                        for group in subcategory["groups"]:
+                            categories_writer.writerow(
+                                [group["id"], group["active"], group["name"], group["parent"], group["is_root"]]
+                            )
+                            process_and_write_products(group, group["link"], limit_of_products_in_group)
+                    elif "products" in subcategory:
+                        process_and_write_products(subcategory, subcategory["link"], limit_of_products_in_group)
 
     return product_categories_data
 
@@ -240,8 +268,7 @@ from utils import sanitize_filename, download_image
 
 def parse_product_page(driver: uc.Chrome, url: str) -> Dict:
     """
-    Parses a single product page. This version is more robust and handles
-    both tea and coffee product page structures.
+    Parses a single product page
     """
     driver.get(url)
     human_wait(0.5, 0.2)
@@ -250,7 +277,8 @@ def parse_product_page(driver: uc.Chrome, url: str) -> Dict:
 
     product_info['id'] = None
     product_info['active'] = 1
-    # Basic Info
+
+
     try:
         product_info['name'] = driver.find_element(By.CSS_SELECTOR, 'h1[itemprop="name"]').text
     except NoSuchElementException:
@@ -266,55 +294,28 @@ def parse_product_page(driver: uc.Chrome, url: str) -> Dict:
         product_info['price_netto'] = None
         logging.warning(f"Could not parse product price for {url}")
 
-    # Main Image and Thumbnails
+
 
     try:
         images_objects = driver.find_elements(By.CSS_SELECTOR, "img.js-thumb")
         product_info['images'] = [img.get_attribute("data-image-large-src") for img in images_objects]
-
-        # product_info['main_image_url'] = driver.find_element(By.CSS_SELECTOR, 'img.js-qv-product-cover').get_attribute(
-        #     'src').replace("home_default", "large_default").replace("webp", "jpg")
-        # thumb_elements = driver.find_elements(By.CSS_SELECTOR, 'ul.product-images img')
-        #
-        # for thumb in thumb_elements:
-        #     temp = thumb.get_attribute('src').replace("home_default", "large_default").replace("webp", "jpg")
-        #     if temp not in product_info['thumbnail_urls']:
-        #         product_info['thumbnail_urls'].append(temp)
-
-        # product_info['thumbnail_urls'] = [thumb.get_attribute('src').replace("home_default", "large_default").replace("webp", "jpg") for thumb in thumb_elements]
-
     except NoSuchElementException:
         product_info['main_image_url'] = ''
         product_info['thumbnail_urls'] = []
         logging.warning(f"Could not parse images for {url}")
 
 
-    # Download images
     product_info["images_path"] = []
     sanitized_product_name = sanitize_filename(product_info.get('name', ''))
     if sanitized_product_name and product_info['images']:
         image_dir = os.path.join('images', 'product_images', sanitized_product_name)
-        
-        # Download main image
-
-
-        # try:
-        #     main_image_url = product_info['main_image_url']
-        #     downloaded_path = download_image(main_image_url, 'main_image', image_dir)
-        #     if downloaded_path:
-        #         product_info['main_image_path'] = downloaded_path
-        # except Exception as e:
-        #     logging.error(f"Error downloading main image for {product_info['name']}: {e}")
-
-        # Download thumbnail images
-        for i,  url in enumerate(product_info.get('images', [])):
+        for i,  image_url in enumerate(product_info.get('images', [])):
             try:
-                downloaded_path = download_image(url.replace("webp", "jpg"), f'img_{i}', image_dir)
+                downloaded_path = download_image(image_url.replace("webp", "jpg"), f'img_{i}', image_dir)
                 if downloaded_path:
                     product_info['images_path'].append(downloaded_path)
             except Exception as e:
                 logging.error(f"Error downloading img {i} for {product_info['name']}: {e}")
-
 
     try:
         product_info['code'] = driver.find_element(By.CSS_SELECTOR, 'span[itemprop="sku"]').text
@@ -322,7 +323,6 @@ def parse_product_page(driver: uc.Chrome, url: str) -> Dict:
         product_info['code'] = ''
         logging.warning(f"Could not parse SKU for {url}")
 
-    # Robust Description Parsing
     product_info.update({
         'full_description_html': '',
         'text_description': '',
@@ -336,13 +336,12 @@ def parse_product_page(driver: uc.Chrome, url: str) -> Dict:
         description_div = driver.find_element(By.CSS_SELECTOR, 'div.product-description')
         product_info['full_description_html'] = description_div.get_attribute('innerHTML').strip()
 
-        # Set a default text description from the first paragraph
+
         try:
             product_info['text_description'] = description_div.find_element(By.TAG_NAME, 'p').text.strip().replace('\n', ' ')
         except NoSuchElementException:
             pass
 
-        # Find all paragraphs and check for keywords
         description_paragraphs = description_div.find_elements(By.TAG_NAME, 'p')
         for p in description_paragraphs:
             p_text = p.text
@@ -391,7 +390,6 @@ def parse_product_page(driver: uc.Chrome, url: str) -> Dict:
     except NoSuchElementException:
         logging.warning(f"Could not parse categories section for {url}")
 
-    # Initialize empty variants
     product_info['variants'] = []
 
     return product_info
@@ -407,11 +405,10 @@ if __name__ == "__main__":
 
     driver.get(BASE_LINK)
     accept_cookies(driver)
-
     try:
-        result = parse(driver, limit_of_groups=3, limit_of_subcategories=4, limit_of_categories=4)
-        print(json.dumps(result, indent=4))
-
+        result = parse(driver)
+        with open('products.json', "w+") as f:
+            f.write(json.dumps(result, indent=4))
 
     finally:
         driver.quit()
