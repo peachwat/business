c32dc24c2d325fdfe96a1d400b1d32fe07af07a0
nikitaksk <nikitaksk1@icloud.com>
2025-12-13 22:27:41 +0100

    temp hook added

--- prestashop_src/modules/topper/topper.php
+++ prestashop_src/modules/topper/topper.php
@@ -1,37 +1,34 @@
 <?php
-
 if (!defined('_PS_VERSION_')) {
     exit;
 }
 
-class MyCustomHooks extends Module
+class Topper extends Module
 {
     public function __construct()
     {
-        $this->name = 'mycustomhooks';
+        $this->name = 'Topper';
         $this->tab = 'front_office_features';
         $this->version = '1.0.0';
-        $this->author = 'Your Name';
+        $this->author = 'nikitaksk';
         $this->need_instance = 0;
         $this->ps_versions_compliancy = [
-            'min' => '1.7.0.0',
-            'max' => _PS_VERSION_,
+            'min' => '1.7',
+            'max' => _PS_VERSION_
         ];
         $this->bootstrap = true;
 
         parent::__construct();
 
-        $this->displayName = $this->l('My Custom Hooks');
-        $this->description = $this->l('A module to register the displayTopper custom hook.');
+        $this->displayName = $this->l('Topper Module');
+        $this->description = $this->l('A module witht the displayTopper hook');
 
         $this->confirmUninstall = $this->l('Are you sure you want to uninstall?');
     }
 
     public function install()
     {
-        if (!parent::install() ||
-            !$this->registerHook('displayTopper')
-        ) {
+        if (!parent::install() || !$this->registerHook('displayTopper')) {
             return false;
         }
         return true;
@@ -39,9 +36,14 @@ class MyCustomHooks extends Module
 
     public function uninstall()
     {
-        if (!parent::uninstall()) {
+        if (!parent::uninstall() || !$this->unregisterHook('displayTopper')) {
             return false;
         }
         return true;
+    }
+
+    public function hookDisplayTopper($params)
+    {
+        return $this->display(__FILE__, 'topper.tpl');
     }
 }
---
+++ prestashop_src/modules/topper/topper.tpl
@@ -0,0 +1,4 @@
+<div class="topper-container" style="background-color:rgb(255, 0, 0); text-align: center;">
+    <h3>ALOOOOOOOOOOOOOOOOOOO</h3>
+    {widget name="ps_searchbar"}
+</div>
