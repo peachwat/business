6efd0464001996a0e7c94a6af027ee49cfe6e70a
Evelina Rylova <evelina1rylova@gmail.com>
2025-12-14 15:38:40 +0100

    Updated banner and added same as on orig site product sections on home page

---
+++ prestashop_src/modules/ps_viewedproduct/vendor/composer/installed.php
@@ -0,0 +1,23 @@
+<?php return array(
+    'root' => array(
+        'pretty_version' => 'dev-master',
+        'version' => 'dev-master',
+        'type' => 'prestashop-module',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'reference' => '18328b54bcff8ed74c05b817046bf9eaddb862d0',
+        'name' => 'prestashop/ps_viewedproduct',
+        'dev' => false,
+    ),
+    'versions' => array(
+        'prestashop/ps_viewedproduct' => array(
+            'pretty_version' => 'dev-master',
+            'version' => 'dev-master',
+            'type' => 'prestashop-module',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'reference' => '18328b54bcff8ed74c05b817046bf9eaddb862d0',
+            'dev_requirement' => false,
+        ),
+    ),
+);
